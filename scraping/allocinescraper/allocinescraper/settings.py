c9b26bace239db16c039c7029741a0854855b4b7
MichAdebayo <adebayomichael4@gmail.com>
2025-04-12 11:02:16 +0200

    allocine first scrapping complete

--- scraping/allocinescraper/allocinescraper/settings.py
+++ scraping/allocinescraper/allocinescraper/settings.py
@@ -16,17 +16,18 @@ SPLASH_URL = 'http://localhost:8050'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "allocinescraper (+http://www.yourdomain.com)"
+USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 20
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 0.25
 LOG_LEVEL = 'INFO'
 
 # The download delay setting will honor only one of:
@@ -40,10 +41,10 @@ LOG_LEVEL = 'INFO'
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
+#    "Accept-Language": "fr-FR",
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -67,6 +68,8 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
 }
 
+
+SPLASH_SLOT_POLICY = 'multiple_slots'
 DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
 HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
@@ -84,11 +87,11 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 1
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 10
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
@@ -97,8 +100,8 @@ ITEM_PIPELINES = {
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 86400
 #HTTPCACHE_DIR = "httpcache"
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
@@ -115,5 +118,3 @@ FEED_EXPORT_FIELDS = ['film_title', 'film_url', 'film_image_url' , 'release_date
                       'awards', 'budget', 'associated_genres', 'press_critics_count',
                       'viewer_critics_count', 'broadcast_category', 'trailer_views', 'synopsis', 
 ]
-
-
--- scraping/allocinescraper/allocinescraper/spiders/allocine.py
+++ scraping/allocinescraper/allocinescraper/spiders/allocine.py
@@ -1,43 +1,41 @@
-import scrapy
+import shutil
 from allocinescraper.items import AllocinescraperItem
+from scrapy_splash import SplashRequest, SlotPolicy
+from scrapy import signals
+import os
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from scrapy_splash import SplashRequest, SlotPolicy
 
 class AllocineSpider(CrawlSpider):
     name = "allocine"
     allowed_domains = ["allocine.fr"]
-    start_urls = ["https://allocine.fr/films/?page=" + str(x) for x in range(1, 8000)]
-    # start_urls = ["https://allocine.fr/films/"]
-
-    custom_settings = {
-        'USER_AGENT': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-        'DOWNLOAD_DELAY': 1,
-        'AUTOTHROTTLE_ENABLED': True,
-        'CONCURRENT_REQUESTS' : 25,
-        #'HTTPCACHE_ENABLED' : True
-    }
+    start_urls = ["https://allocine.fr/films/?page=" + str(x) for x in range(1, 3000)]
 
     rules = (
         Rule(LinkExtractor(restrict_xpaths=".//a[@class='meta-title-link']"), callback='parse_film'),
     )
 
-    # rules = (
-    #     Rule(LinkExtractor(restrict_css='.pagination a'), follow=True, process_request='limit_pages'),
-    #     Rule(LinkExtractor(restrict_css='.meta-title-link'), callback='parse_film'),    
-    # )
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(AllocineSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_closed, signals.spider_closed)
+        return spider
 
-    # max_pages = 2
-    # pages_scraped = 0
+    def spider_closed(self, spider):
+        # Auto-delete cache folder after spider run
+        cache_dir = self.settings.get('HTTPCACHE_DIR', 'httpcache')
+        if os.path.exists(cache_dir):
+            try:
+                shutil.rmtree(cache_dir)
+                self.logger.info("HTTP cache folder '%s' has been removed.", cache_dir)
+            except Exception as e:
+                self.logger.error("Failed to remove HTTP cache folder '%s': %s", cache_dir, str(e))
+        else:
+            self.logger.info("No HTTP cache folder found to remove.")
 
-    # def limit_pages(self, request, *args):
-    #     if self.pages_scraped >= self.max_pages:
-    #         self.logger.info(f'Stopping after {self.max_pages} pages.')
-    #         return None
-    #     self.pages_scraped += 1
-    #     return request
 
     def parse_film(self, response):
+        self.logger.info(f"Scraping film page: {response.url}")
         item = AllocinescraperItem()
         item['film_title'] = response.css('div.titlebar-title::text').get('').strip()
         item['film_url'] = response.url
@@ -87,97 +85,76 @@ class AllocineSpider(CrawlSpider):
         item['distributor'] = response.xpath('//div[contains(@class, "item")][.//span[contains(@class, "what") and contains(text(), "Distributeur")]]//span[contains(@class, "blue-link")]/text()').get(default='').strip()
 
         year_of_production_str = response.xpath('//div[contains(@class, "item")][.//span[contains(@class, "what") and contains(text(), "Année de production")]]//span[contains(@class, "that")]/text()').get()
-
         try:
             year_of_production = int(year_of_production_str) if year_of_production_str else None
             item['year_of_production'] = year_of_production
-
             if year_of_production and year_of_production < 2000:
                 self.logger.info(f"Skipping movie {item.get('film_title')} (year: {year_of_production})")
-                return  # Skip this movie
-
+                return
         except ValueError:
             self.logger.warning(f"Invalid year format: {year_of_production_str}")
-            item['year_of_production'] = None # Set to None if the year is invalid
-            return # Skip this movie
+            item['year_of_production'] = None
+            return
         
         item['film_nationality'] = response.xpath('//div[contains(@class, "item")][.//span[contains(@class, "what") and contains(text(), "Nationalités")]]//span[contains(@class, "nationality")]/text()').getall()
         item['filming_secrets'] = response.xpath('//div[contains(@class, "item")][.//span[contains(@class, "what") and contains(text(), "Secrets de tournage")]]//span[contains(@class, "that")]/text()').get()
         item['awards'] = response.xpath('//div[contains(@class, "item")][.//span[contains(@class, "what") and contains(text(), "Récompenses")]]//span[contains(@class, "that")]/text()').get()
         item['budget'] = response.xpath('//div[contains(@class, "item")][.//span[contains(@class, "what") and contains(text(), "Budget")]]//span[contains(@class, "that")]/text()').get()
         
-
+        # Process box-office and trailer next via Splash
         film_box_office_url = response.url.replace('_gen_cfilm=', '-').replace('.html', '/') + 'box-office/'
         
         yield SplashRequest(
             url=film_box_office_url,
             callback=self.parse_box_office_page,
             meta={'item': item},
-            args={'wait': 1},  # you can adjust the wait time as needed
+            endpoint='render.html',
+            slot_policy=SlotPolicy.PER_DOMAIN,
             dont_filter=True
-)
+        )
+
     def parse_box_office_page(self, response):
         item = response.meta['item']
-
-        # Locate sections by header text rather than by index
         france_section = response.xpath('//section[contains(.//h2/text(), "Box Office France")]')
         us_section = response.xpath('//section[contains(.//h2/text(), "Box Office US")]')
 
-        if france_row := france_section.xpath(
-            './/table[contains(@class, "box-office-table")]/tbody/tr[1]'
-        ):
-            self.extract_entry_and_entry_week(
-                france_row, item, 'fr_entry_week', 'fr_entries'
-            )
+        if france_row := france_section.xpath('.//table[contains(@class, "box-office-table")]/tbody/tr[1]'):
+            self.extract_entry_and_entry_week(france_row, item, 'fr_entry_week', 'fr_entries')
         else:
             item['fr_entry_week'] = item['fr_entries'] = ''
 
-        if us_row := us_section.xpath(
-            './/table[contains(@class, "box-office-table")]/tbody/tr[1]'
-        ):
-            self.extract_entry_and_entry_week(
-                us_row, item, 'us_entry_week', 'us_entries'
-            )
+        if us_row := us_section.xpath('.//table[contains(@class, "box-office-table")]/tbody/tr[1]'):
+            self.extract_entry_and_entry_week(us_row, item, 'us_entry_week', 'us_entries')
         else:
             item['us_entry_week'] = item['us_entries'] = ''
 
         trailer_relative_url = response.xpath('//div[contains(@class, "roller-slider")]//a[contains(@class, "trailer roller-item")]/@href').get()
-        
-        if trailer_url := f'https://www.allocine.fr{trailer_relative_url}':
+        if trailer_relative_url:
+            trailer_url = f'https://www.allocine.fr{trailer_relative_url}'
             yield SplashRequest(
                 url=trailer_url,
                 callback=self.parse_trailer_page,
                 errback=self.handle_splash_error,
                 meta={'item': item},
-                args={'wait': 2},
                 endpoint='render.html',
-                slot_policy=SlotPolicy.PER_DOMAIN, # Fix for ScrapyDeprecationWarning
+                slot_policy=SlotPolicy.PER_DOMAIN,
                 dont_filter=True
             )
         else:
             self.logger.warning(f"No trailer URL found for {item.get('film_title', 'unknown movie')}")
-            yield item  # Yield the item even if there's no trailer
-        
+            yield item
+
     def parse_trailer_page(self, response):
         item = response.meta['item']
-
         trailer_views = response.xpath('//div[contains(@class, "media-info-item-holder")]//div[contains(@class, "icon-eye")]/text()').get()
         item['trailer_views'] = trailer_views.strip().replace(' ', ',') if trailer_views else ""
-        
         yield item
 
-    def extract_entry_and_entry_week(self, arg0, item, arg2, arg3):
-        fr_week_element = arg0.css('td.responsive-table-column.first-col span::text')
-        item[arg2] = fr_week_element.get().strip() if fr_week_element else None
-        item[arg3] = (
-            arg0.xpath('.//td[@data-heading="Entrées"]/text()')
-            .get(default='')
-            .strip()
-        )
+    def extract_entry_and_entry_week(self, selector, item, key_week, key_entries):
+        fr_week_element = selector.css('td.responsive-table-column.first-col span::text')
+        item[key_week] = fr_week_element.get().strip() if fr_week_element else None
+        item[key_entries] = selector.xpath('.//td[@data-heading="Entrées"]/text()').get(default='').strip()
 
     def handle_splash_error(self, failure):
         self.logger.error(f"Error with Splash: {repr(failure)}")
-        request = failure.request
-        yield request.meta['item']
-
-        
+        yield failure.request.meta['item']
