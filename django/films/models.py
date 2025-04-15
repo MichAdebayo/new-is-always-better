591d429c85147fbcf49ee8cdc98d636da3c5a8e5
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-04-15 13:05:33 +0200

    use only 'films_jp_box_V2.csv' file if you import csv via settings men

--- django/films/models.py
+++ django/films/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+DATE_FORMAT_STRING = "%d/%m/%Y"
+
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     image_url = models.URLField()
     synopsis = models.TextField()
     genre = models.CharField(max_length=100)
     cast = models.TextField()
+    release_date_fr = models.DateField(null=True, default=None)
     actual_entries_france= models.IntegerField()
     room = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,9 +18,9 @@ class Movie(models.Model):
 
 class PredictionHistory(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='predictions')
-    first_week_predicted_entries_france = models.IntegerField()
-    metric_score = models.FloatField()
-    model_version = models.IntegerField()
+    first_week_predicted_entries_france = models.IntegerField(default=0)
+    prediction_error = models.IntegerField(default=0)
+    model_version = models.IntegerField(default=0)
     date = models.DateField()
    
     def __str__(self):
--- django/films/views.py
+++ django/films/views.py
@@ -1,69 +1,77 @@
 import os
 from django.contrib import messages
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
-from .models import Movie, PredictionHistory
-from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
+from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Movie, PredictionHistory, DATE_FORMAT_STRING
+
 import csv
+import datetime as dt
 
-
+#__________________________________________________________________________________________________
+#
+# region dashboard
+#__________________________________________________________________________________________________
 def dashboard(request):
     
-    selected_movies = Movie.objects.prefetch_related('predictions').all()[:2]  # Les deux premiers films
-    upcoming_movies = Movie.objects.prefetch_related('predictions').all()[2:]  # Tous les films à venir
+    today = dt.datetime.now()
+    limit_date =  today - dt.timedelta(days=14)
 
-    predictions = []
-    for movie in selected_movies :
+    recent_movies = Movie.objects.filter(  
+            release_date_fr__gt = limit_date
+        ).order_by(
+            '-release_date_fr'
+        ).prefetch_related('predictions').all()
+
+    for movie in recent_movies :
         predictions = movie.predictions.all()
         if len(predictions) >0 :
             movie.last_prediction = predictions.last().first_week_predicted_entries_france
 
-    for movie in upcoming_movies :
-        predictions = movie.predictions.all()
-        if len(predictions) >0 :
-            movie.last_prediction = predictions.last().first_week_predicted_entries_france
+    selected_movies = recent_movies[:2]
+    upcoming_movies = recent_movies[2:] 
     
     context = {
         'selected_movies': selected_movies, 
-        'upcoming_movies': upcoming_movies,
-        'predictions': predictions,
+        'upcoming_movies': upcoming_movies
     }
 
     return render(request, 'films/dashboard.html', context)
 
+#__________________________________________________________________________________________________
+#
+# region run_movie_prediction
+#__________________________________________________________________________________________________
 def run_movie_prediction(request, movie_id) :
     movie = get_object_or_404(Movie, id=movie_id)
 
-    # chargement modèle
-    import joblib
-    from sklearn.pipeline import Pipeline
-    dummy_model: Pipeline  = joblib.load("dummy_pipeline.joblib")
+    model_version = 0
 
-    # chargement data 
-    import pandas as pd
-    original_csv = pd.read_csv('static/films_jp_box.csv')
+    from first_predictor import FirstPredictor
+    predictor = FirstPredictor(model_version)
+    (prediction, error) = predictor.predict(movie.title, "")
     
-    movie_row = original_csv[original_csv['titre']== movie.title].iloc[[0]]
-    prediction = dummy_model.predict(movie_row)
-
-    pred_int = int(prediction[0])
     import datetime as dt
-
     today = dt.datetime.now().date()
 
     new_entry = PredictionHistory(
         movie_id = movie.id, 
-        first_week_predicted_entries_france = pred_int, 
-        metric_score = float(pred_int/4),
-        model_version = 0,
+        first_week_predicted_entries_france = prediction, 
+        prediction_error = error,
+        model_version = predictor.model_version,
         date = today
     )
     new_entry.save()
 
-    messages.success(request, f"Action launched for the film: {movie.title}")
+    messages.success(request, f"Action launched for the movie: {movie.title}")
     return redirect('dashboard')  
 
+#__________________________________________________________________________________________________
+#
+# region history
+#__________________________________________________________________________________________________
 def history(request):
     prediction_history = PredictionHistory.objects.all().order_by('date')
     return render(request, 'films/history.html', {
@@ -71,6 +79,10 @@ def history(request):
         'active_tab': 'history'
     })
 
+#__________________________________________________________________________________________________
+#
+# region financials
+#__________________________________________________________________________________________________
 def financials(request):
     weekly_revenue = 9000
     weekly_costs = 4900
@@ -89,9 +101,17 @@ def financials(request):
         'active_tab': 'financials'
     })
 
+#__________________________________________________________________________________________________
+#
+# region settings
+#__________________________________________________________________________________________________
 def settings(request):
     return render(request, 'films/settings.html')
 
+#__________________________________________________________________________________________________
+#
+# region import_csv
+#__________________________________________________________________________________________________
 def import_csv(request):
     if request.method == 'POST' and request.FILES.get('csv_file'):
         try:
@@ -114,15 +134,20 @@ def import_csv(request):
 
             # Lire le fichier CSV et remplir la base de données
             with open(file_path, newline='', encoding='utf-8') as file:
-                reader = csv.DictReader(file)
-                print("Headers:", reader.fieldnames)  # Affiche les noms des colonnes
+                #reader = csv.DictReader(file )
+                reader = csv.DictReader(file, delimiter=';')
+                #print("Headers:", reader.fieldnames)  # Affiche les noms des colonnes
 
                 movie_count = 0
                 for row in reader:
-                    print(row)  # Affiche les données de chaque ligne
+                    #print(row)  # Affiche les données de chaque ligne
 
                     try:
-                        total_entries = int(str(row['entrees_totales_france']).replace(' ', ''))
+                        
+                        total_entries_str = str(row['entrees_totales_france']).replace(' ', '')
+                        total_entries =  int(total_entries_str) if isinstance (total_entries_str, int)  else 0
+
+                        release_date_france = dt.datetime.strptime(row['date_sortie_france'], DATE_FORMAT_STRING)
 
                         movie = Movie(
                             title=row['titre'],
@@ -131,11 +156,12 @@ def import_csv(request):
                             genre=row['genre_principale'],
                             cast=row['acteurs'], 
                             actual_entries_france = total_entries, 
+                            release_date_fr = release_date_france
                         )
                         movie.save()
                         movie_count += 1
                     except Exception as movie_error:
-                        print(f"Error inserting movie: {movie_error}")
+                        print(f"Error inserting movie : {row['titre']}, {release_date_france}")
                         continue  # Continue même si une ligne échoue
 
             # Afficher un message de succès
